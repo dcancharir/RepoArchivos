4af3e8db603f86f5477b954821926d30f36ae33c
Diego Canchari <d.cancharir@gmail.com>
2020-04-27 12:37:48 -0500

    Inicializar Repositorio

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,105 @@
+﻿importScripts('/Content/assets/firebase-app.js');
+importScripts('/Content/assets/firebase-messaging.js');
+
+// Initialize the Firebase app in the service worker by passing in the
+// messagingSenderId.
+firebase.initializeApp({
+    'messagingSenderId': '55411354370'
+});
+
+// Retrieve an instance of Firebase Messaging so that it can handle background
+// messages.
+const messaging = firebase.messaging();
+
+messaging.setBackgroundMessageHandler(function (payload) {
+    console.log('[firebase-messaging-sw.js] Received background message ', payload);
+    // Customize notification here
+    const notificationTitle = payload.data.title;
+    const notificationOptions = {
+        body: payload.data.body,
+        badge: '/Content/assets/images/badge.png',
+        icon: '/Content/assets/images/logo-white-small.png',
+        click_action: payload.data.click_action,
+        vibrate: [100, 50, 100],
+    };
+
+    return self.registration.showNotification(notificationTitle,
+        notificationOptions);
+});
+
+var CACHE_NAME = 'asistencia-index';
+var urlsToCache = [
+
+    '/Content/assets/css/vendor.styles.css',
+    '/Content/assets/css/demo/dark-template.css',
+    '/Content/assets/js/components/pnotify/pnotify.css',
+    '/Content/assets/js/components/pnotify/pnotify.buttons.css',
+];
+
+self.addEventListener('install', function (event) {
+    // Perform install steps
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(function (cache) {
+                console.log('Opened cache');
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+self.addEventListener('fetch', function (event) {
+    event.respondWith(
+        caches.match(event.request)
+            .then(function (response) {
+                // Cache hit - return response
+                if (response) {
+                    return response;
+                }
+
+                // IMPORTANT: Clone the request. A request is a stream and
+                // can only be consumed once. Since we are consuming this
+                // once by cache and once by the browser for fetch, we need
+                // to clone the response.
+                var fetchRequest = event.request.clone();
+
+                return fetch(fetchRequest).then(
+                    function (response) {
+                        // Check if we received a valid response
+                        if (!response || response.status !== 200 || response.type !== 'basic') {
+                            return response;
+                        }
+
+                        // IMPORTANT: Clone the response. A response is a stream
+                        // and because we want the browser to consume the response
+                        // as well as the cache consuming the response, we need
+                        // to clone it so we have two streams.
+                        var responseToCache = response.clone();
+
+                        caches.open(CACHE_NAME)
+                            .then(function (cache) {
+                                cache.put(event.request, responseToCache);
+                            });
+
+                        return response;
+                    }
+                );
+            })
+    );
+});
+
+self.addEventListener('activate', function (event) {
+
+    var cacheWhitelist = ['asistencia-index', 'blog-posts-cache-v1'];
+
+    event.waitUntil(
+        caches.keys().then(function (cacheNames) {
+            return Promise.all(
+                cacheNames.map(function (cacheName) {
+                    if (cacheWhitelist.indexOf(cacheName) === -1) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
---
+++ firebase/script-firebase.js
@@ -0,0 +1,77 @@
+﻿if ('serviceWorker' in navigator) {
+
+    navigator.serviceWorker.register(basePath + 'firebase-messaging-sw.js')
+        .then(function (registration) {
+            // Initialize the Firebase app by passing in the messagingSenderId
+            var config = {
+                apiKey: "apikey",
+                authDomain: "dominio firebase proyecto",
+                databaseURL: "url firebase proyecto",
+                projectId: "id",
+                storageBucket: "storage",
+                messagingSenderId: "id sender",
+                appId: "id"
+            };
+            firebase.initializeApp(config);
+            const messaging = firebase.messaging();
+            messaging.useServiceWorker(registration);
+            console.log('ServiceWorker registration fireabase succesufull with scope: ', registration.scope);
+            // Request for permission
+            messaging.requestPermission()
+                .then(function () {
+                    console.log('Notification permiso concedido.');
+                    // TODO(developer): Retrieve an Instance ID token for use with FCM.
+                    messaging.getToken()
+                        .then(function (currentToken) {
+                            if (currentToken) {
+                                console.log('Token: ' + currentToken);
+                                // codigo que tiene qe ejecutarse si me generan un token 
+                            } else {
+                                console.log('respondio con un token vacio. necesita permiso para generar uno.');
+                            }
+                        })
+                        .catch(function (err) {
+                            console.log('Ocurrio un error al solicitar el token. ', err);
+                        });
+                })
+                .catch(function (err) {
+                    console.log('no se pudo conseguir el permiso para notificar.', err);
+                });
+
+            // Handle incoming messages
+            messaging.onMessage(function (payload) {
+                console.log("Notification recibida: ", payload);
+                const Title = payload.data.title;
+                const options = {
+                    body: payload.data.body,
+                    badge: '/Content/assets/images/logo32.png',
+                    icon: '/Content/assets/images/logo-white-small.png',
+                    click_action: payload.data.click_action,
+                    vibrate: [100, 50, 100],
+
+                };
+                registration.showNotification(Title, options);
+            });
+
+            // Callback fired if Instance ID token is updated.
+            messaging.onTokenRefresh(function () {
+                messaging.getToken()
+                    .then(function (refreshedToken) {
+                        console.log('Token actualizado.');
+                        
+                        // si se refresca que hacer con el nuevo token
+                        
+
+                    })
+                    .catch(function (err) {
+                        console.log('no se pudo conseguir actualizar el token ', err);
+                    });
+            });
+        });
+
+} else console.log('Tu navegador no soporta the Service-Worker!');
+
+
+
+
+
